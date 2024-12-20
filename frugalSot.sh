ee7468003f7d1eb018eddb0ea45b747ede58dba4
Pradheep P <vidhyaprakash2510@gmail.com>
2024-12-21 03:17:38 +0530

    small fix in frugal sot

--- frugalSot.sh
+++ frugalSot.sh
@@ -6,6 +6,9 @@ filename=data/output.txt
 data1=$(<"$filename")
 PROMPT=$(grep -o '"prompt": *"[^"]*"' data/test.txt | sed 's/"prompt": "//; s/"$//')
 
+echo "Prompt: $PROMPT"
+echo "Data: $data1"
+
 similarity_score=$(python3 textSimilarity.py "$data1" "$PROMPT")
 
 echo "Similarity score: $similarity_score"

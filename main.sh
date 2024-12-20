162eb57968a14bc47fd374d7883d7f79978cb13d
Pradheep P <vidhyaprakash2510@gmail.com>
2024-12-20 18:59:07 +0530

    inference time diff in ms is fixed in shell scripts

--- main.sh
+++ main.sh
@@ -13,19 +13,14 @@ echo "Start time: $start_time"
 python3 main.py "$PROMPT"
 
 COMPLEXITY=$(grep -o '"complexity": *"[^"]*"' data/test.txt | sed 's/"complexity": "//; s/"$//')
-# PROMPT=$(grep -o '"prompt": *"[^"]*"' data/test.txt | sed 's/"prompt": "//; s/"$//')
 
 echo "Complexity: $COMPLEXITY"
 echo "Prompt: $PROMPT"
 
-# python prompt.py $PROMPT
-
 case "$COMPLEXITY" in
     "Low")
-        echo start="$(date +%s)"
         echo "Running low complexity model..."
         ollama run tinyllama "$PROMPT"
-        echo end="$(date +%s)"
         ;;
     "Mid")
         echo "Running medium complexity model..."
@@ -36,7 +31,7 @@ case "$COMPLEXITY" in
         ollama run gemma2:2b "$PROMPT"
         ;;
     *)
-        echo "Unkown complexity level: $COMPLEXITY"
+        echo "Unknown complexity level: $COMPLEXITY"
         echo "Running on highly inefficient model..."
         ollama run phi "$PROMPT"
         ;;
@@ -44,8 +39,9 @@ esac
 
 end_time=$(date +%s.%3N)
 echo "End time: $end_time"
-time_diff_ms=$(bc -l <<< "scale=0; ($end_time - $start_time) * 1000")
+time_diff_ms=$(awk "BEGIN {printf \"%.0f\", ($end_time - $start_time) * 1000}")
 echo "Time taken: $time_diff_ms ms"
 echo "Done!!"
+
 # now the remaining process is to capture and store it in the vector db or direct storage
 

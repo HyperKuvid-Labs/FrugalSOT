bce166eacd1ffe5caac160ee4b487e91fcdcd52e
Pradheep P <130392782+Mantissagithub@users.noreply.github.com>
2025-03-09 02:08:11 +0530

    Update modelThing.sh

--- scripts/modelThing.sh
+++ scripts/modelThing.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+
 total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 total_ram_gb=$(echo "scale=2; $total_ram_kb / 1024 / 1024" | bc)
 echo "Total RAM: $total_ram_gb GB"
 
 Models=$(python ../src/modelInitialization.py "$total_ram_gb")
 
-Low_model=$(echo "Low Model" | jq -r '.Low')
-Mid_model=$(echo "Mid Model" | jq -r '.Mid')
-High_model=$(echo "High Model" | jq -r '.High')
-Unknown_model=$(echo "Unknown Model" | jq -r '.Unknown')
+Low_model=$(echo "$Models" | jq -r '.Low')
+Mid_model=$(echo "$Models" | jq -r '.Mid')
+High_model=$(echo "$Models" | jq -r '.High')
+Unknown_model=$(echo "$Models" | jq -r '.Unknown')
 
 echo "Low Model: $Low_model"
 echo "Mid Model: $Mid_model"
 echo "High Model: $High_model"
 echo "Unknown Model: $Unknown_model"
 
+ollama run "$Low_model" what is ai?

e402b7666545beb5cb14abfa69f0799d0ef67f58
Pradheep P <vidhyaprakash2510@gmail.com>
2025-03-09 01:35:41 +0530

    enhanced the checking of model initialization

--- scripts/modelThing.sh
+++ scripts/modelThing.sh
@@ -2,4 +2,15 @@ total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 total_ram_gb=$(echo "scale=2; $total_ram_kb / 1024 / 1024" | bc)
 echo "Total RAM: $total_ram_gb GB"
 
-python ../src/modelInitialization.py "$total_ram_gb"
+Models=$(python ../src/modelInitialization.py "$total_ram_gb")
+
+Low_model=$(echo "Low Model" | jq -r '.Low')
+Mid_model=$(echo "Mid Model" | jq -r '.Mid')
+High_model=$(echo "High Model" | jq -r '.High')
+Unknown_model=$(echo "Unknown Model" | jq -r '.Unknown')
+
+echo "Low Model: $Low_model"
+echo "Mid Model: $Mid_model"
+echo "High Model: $High_model"
+echo "Unknown Model: $Unknown_model"
+
--- src/modelInitialization.py
+++ src/modelInitialization.py
@@ -6,8 +6,11 @@ def modelsInitialization(ramGB):
     # print(data)
     if ramGB>= 8:
         print(data["MoreThan8GB"])
+        data1 = data
     else:
         print(data["LessThan8gb"])
+        data1 = data
+    return data1
     
 
 # modelsInitialization(7.75)
